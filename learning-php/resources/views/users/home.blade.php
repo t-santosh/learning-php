8438bea5ee49e91ef9a28a45ca4e7dba2f81e2a3
Santosh Tamang <santoshtamang@st161726-workStation.local>
2022-05-21 19:00:46 +1000

    dashboard update

---
+++ learning-php/resources/views/users/home.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+    <p>Feel free to edit this dashboard</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop`
